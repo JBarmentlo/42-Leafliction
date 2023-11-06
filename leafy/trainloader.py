6c5f653f0bafd843efd85869d9c5b0ea4de18d36
Joep B <joepbarmentlo@gmail.com>
2023-11-06 20:22:14 +0100

    train with resnet seem good no split yet

--- leafy/trainloader.py
+++ leafy/trainloader.py
@@ -10,14 +10,14 @@ from typing import List, Tuple
 
 from .loader import ImageLoader
 
-class TrainLoader(Dataset):
+class ImageDataset(Dataset):
     def __init__(self, image_loader: ImageLoader):
         self.image_loader = image_loader
         self.images: Tensor
         self.ground_truth_str = []
         self.class_labels = self.compute_class_list()
         gt = Tensor(list(map(lambda label: self.class_labels.index(label), self.ground_truth_str)))
-        self.gt = F.one_hot(gt.to(torch.int64), num_classes=len(self.class_labels))
+        self.gt = F.one_hot(gt.to(torch.int64), num_classes=len(self.class_labels)).to(torch.float32)
     
     def compute_class_list(self) -> List[str]:
         labels = set()
@@ -27,7 +27,7 @@ class TrainLoader(Dataset):
             self.ground_truth_str.append(label)
             images.append(im)
         
-        self.images = torch.concat(images)
+        self.images = torch.stack(images, dim=0)
         return list(labels)
 
     def __len__(self) -> int:
